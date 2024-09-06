68755a315db30dfcd897db210b9b42706219c67d
jason <json329731049@gmail.com>
2024-09-06 14:40:35 +0800

    把请求的服务器网址写入配置中

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    BASE_URL: 'https://127.0.0.1:8000'
+  },
+  production: {
+    BASE_URL: 'http://43.134.78.200:8800'
+  }
+};
+
+export default config;
--- src/api/httpApi.js
+++ src/api/httpApi.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
+import config from '../../config';
 
-// const BASE_URL = 'http://192.168.5.255:8080';
-// const BASE_URL = 'https://127.0.0.1:8000';
-// const BASE_URL = 'https://127.0.0.1:8000';
-const BASE_URL = 'http://43.134.78.200:8800';
-
+const env = process.env.NODE_ENV || 'development';
+const BASE_URL = config[env].BASE_URL;
 
 // 发送游戏状态到Flask服务器
-export let sendGameStateToFlask = (gameState) => {
-    return axios.post(`${BASE_URL}/ask_poe`, gameState);
-  };
-  
+export const sendGameStateToFlask = (gameState) => 
+  axios.post(`${BASE_URL}/ask_poe`, gameState);
